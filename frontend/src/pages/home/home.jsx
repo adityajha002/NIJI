1d5b83e3d9eaca10bfaff8fd48b4f5585d0e5c5e
adityajha002 <adityajhaofficial02@gmail.com>
2026-07-31 20:05:35 +0530

    Update home.jsx

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -5,12 +5,11 @@ import Section2 from '../../components/section2/section2.jsx'
 import Footer from '../../components/footer/footer.jsx'
 import style from './home.module.css'
 import SideMenu from '../../components/sideMenu/sideMenu.jsx'
-throw new Error("VERCEL TEST");
 function Home() {
   const [sideMenu, setSideMenu] = useState(false);
   return (
     <div className={style.main}>
-      {/* <Nav setSideMenu={setSideMenu}/> */}
+      <Nav setSideMenu={setSideMenu}/> 
       <div className={style.filmWrapper}>
         <div className={style.film}>
           <Section1 />  
