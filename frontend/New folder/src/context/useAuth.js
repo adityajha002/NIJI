6ae284a95e7aeedabf265fc2b51ccdce2e2b55a3
adityajha002 <adityajhaofficial02@gmail.com>
2026-07-31 15:23:22 +0530

    Delete frontend/New folder/src directory

--- frontend/New folder/src/context/useAuth.js
+++
@@ -1,9 +0,0 @@
-import {useContext} from 'react';
-import { AuthContext } from './authContext';
-
-const useAuth = () =>{
-      return useContext(AuthContext);
-}
-
-export { useAuth };
-export default useAuth;
