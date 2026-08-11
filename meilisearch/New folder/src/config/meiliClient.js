69b152e07b9f50089289da84a3f29b207f8820ac
adityajha002 <adityajhaofficial02@gmail.com>
2026-08-11 23:14:10 +0530

    Delete meilisearch/New folder directory

--- meilisearch/New folder/src/config/meiliClient.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import { Meilisearch } from "meilisearch";
-
-export const meiliClient = new Meilisearch({
-  host: process.env.MEILI_HOST,
-  apiKey: process.env.MEILI_MASTER_KEY,
-});
