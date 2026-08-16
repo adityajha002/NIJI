74e3417d0fa80dcb053c62335d0a11e37f866048
adityajha002 <adityajhaofficial02@gmail.com>
2026-08-16 14:12:46 +0530

    Fix import paths for Footer, style, and SideMenu

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import NearbyShops from './components/NearbyShops/NearbyShops';
 import WhyNiji from './components/WhyNiji/WhyNiji';
-import Footer from '../../components/Footer/Footer';
-import style from './Home.module.css';
-import SideMenu from '../../components/SideMenu/SideMenu';
+import Footer from '../../components/footer/footer';
+import style from './home.module.css';
+import SideMenu from '../../components/sideMenu/sideMenu';
 
 function Home() {
   const [sideMenu, setSideMenu] = useState(false);
